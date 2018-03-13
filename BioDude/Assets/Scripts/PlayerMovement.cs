28bd3033bd659787ce80e84633ef09ab60d90385
Ramunas9 <ramskis9@gmail.com>
2018-03-13 19:46:32 +0200

    [GAME-17]: Add sprite, movment and looking script

---
+++ BioDude/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+    private Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        Looking();
+	}
+
+    private void Movement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(moveX, moveY);
+        rb2D.AddForce(move * speed);
+    }
+
+    private void Looking()
+    {
+        Vector2 playerPos = Camera.main.WorldToViewportPoint(transform.position);
+        Vector2 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        float angle = AngleBetweenToPoints(playerPos, mousePos);
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle+90));
+    }
+
+    private float AngleBetweenToPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
