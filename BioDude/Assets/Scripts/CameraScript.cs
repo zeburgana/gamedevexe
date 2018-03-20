85263be426af862cca52ed4b77618ecfefd2b5c3
Ramunas9 <ramskis9@gmail.com>
2018-03-20 15:50:24 +0200

    Modified gitignore

---
+++ BioDude/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    [SerializeField]
+    float maxMovForce = 10f;
+    [SerializeField]
+    GameObject Player;
+    [SerializeField]
+    float smooth = 0.01f;
+    [SerializeField]
+    float TestingForce;
+
+    Rigidbody2D rb2D;
+    
+
+	// Use this for initialization
+	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        Vector3 playerPos = Player.transform.position;
+        Vector3 cameraPos = transform.position;
+        playerPos.z = cameraPos.z;
+        transform.position = Vector3.Lerp(cameraPos, playerPos, smooth);
+	}
+
+    void FixedUpdate()
+    {
+        applyForce();
+    }
+
+    // add force to camera (for ex.: recoil)
+    public void addForce (Vector3 direction, float power)
+    {
+        direction.Normalize();
+        direction.z = 0;
+        rb2D.AddForce(direction * power);
+    }
+
+
+    //FOR TESTING
+    void applyForce()
+    {
+        if (Input.GetKeyDown("F"))
+        {
+            Vector2 playerPos = Camera.main.WorldToViewportPoint(transform.position);
+            Vector2 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            addForce(playerPos - mousePos, TestingForce);
+        }
+    }
+}
