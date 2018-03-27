3b026fa6e55c1b7a577a304a273af6fb14b28653
Aidas Subačius <aidaso2@yahoo,com>
2018-03-27 15:34:47 +0300

    GAME-19
    Added player follow function on BasicEnemy script, so when player is in range enemy follows it, and when player leaves the range enemy continues his walking.

--- BioDude/Assets/Scripts/BasicEnemy.cs
+++ BioDude/Assets/Scripts/BasicEnemy.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicEnemy : MonoBehaviour {
+public class BasicEnemy : MonoBehaviour
+{
 
     private Animator animator;
     private Rigidbody2D RGB;
@@ -28,10 +29,15 @@ public class BasicEnemy : MonoBehaviour {
     private bool IsMoving;
     [SerializeField]
     private int moveX;
+    private int lastMoveX;
     [SerializeField]
     private int moveY;
+    private int lastMoveY;
 
+    private int direction = 0;
 
+    [SerializeField]
+    private bool alerted;
 
     private void Awake()
     {
@@ -40,67 +46,111 @@ public class BasicEnemy : MonoBehaviour {
         CircleCol = GetComponent<CircleCollider2D>();
     }
     // Use this for initialization
-    private void Start () {
+    private void Start()
+    {
         //Equip(0, 0, 0, 0, 0, 0, 0);
         IsMoving = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         /*RaycastHit2D collisionX = Physics2D.Raycast(transform.position, new Vector2(moveX, 0), 0.15f);
         RaycastHit2D collisionY = Physics2D.Raycast(transform.position, new Vector2(0, moveY), 0.16f);
-        
-        if(collisionX.collider.tag == "Wall")
-        {
-            if (moveX == 1)
-            {
-                moveX = -1;
-            }
-            else
-            {
-                moveX = 1;
-            }
-        }
-        if (collisionY.collider.tag == "Wall")
-        {
-            if (moveY == 1)
-            {
-                moveY = -1;
-            }
-            else
-            {
-                moveY = 1;
-            }
-        }*/
+        */
         Move(moveX, moveY);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.collider.tag == "Wallmap")
+        if (collision.gameObject.tag == "Player")
         {
-            if (moveX == 1)
+            alerted = true;
+            lastMoveX = moveX;
+            lastMoveY = moveY;
+        }
+            
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            alerted = false;
+            //moveX = lastMoveX;
+            //moveY = lastMoveY;
+        }
+            
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (alerted && collision.gameObject.tag == "Player")
+        {
+            followPlayer(collision.gameObject.transform.position.x, collision.gameObject.transform.position.y);
+        }
+    }
+    private void followPlayer(float x, float y)
+    {
+        if (Mathf.Abs(transform.position.x - x) > Mathf.Abs(transform.position.y - y))
+        {
+            if (transform.position.x > x)
             {
                 moveX = -1;
+                moveY = 0;
             }
             else
             {
                 moveX = 1;
+                moveY = 0;
             }
         }
-        /*if (collision.collider.tag == "Wall")
+        else
         {
-            if (moveY == 1)
+            if (transform.position.y > y)
             {
                 moveY = -1;
+                moveX = 0;
             }
             else
             {
                 moveY = 1;
+                moveX = 0;
             }
-        }*/
+        }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!alerted)
+        {
+            if (direction == 3 || direction == 4)
+                if (collision.collider.tag == "Wallmap")
+                {
+                    if (moveX == 1)
+                    {
+                        moveX = -1;
+                    }
+                    else
+                    {
+                        moveX = 1;
+                    }
+                }
+            if (direction == 1 || direction == 2)
+                if (collision.collider.tag == "Wallmap")
+                {
+                    if (moveY == 1)
+                    {
+                        moveY = -1;
+                    }
+                    else
+                    {
+                        moveY = 1;
+                    }
+                }
+        }
+    }
+
 
     //Put item id's inside, if id == 0, there is no gear
     public void Equip(int head, int body, int leg, int foot, int back, int left, int right)
@@ -127,21 +177,25 @@ public class BasicEnemy : MonoBehaviour {
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
         SendAnimInfo(1, 0);
+        direction = 1;
     }
     private void walkDown()
     {
         transform.Translate(Vector2.down * speed * Time.deltaTime);
         SendAnimInfo(-1, 0);
+        direction = 2;
     }
     private void walkLeft()
     {
         transform.Translate(Vector2.left * speed * Time.deltaTime);
         SendAnimInfo(0, -1);
+        direction = 3;
     }
     private void walkRight()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
         SendAnimInfo(0, 1);
+        direction = 4;
     }
 
 
@@ -172,6 +226,7 @@ public class BasicEnemy : MonoBehaviour {
                 break;
         }
         IsMoving = true;
-        
+
     }
+ 
 }
