e4e411cfebb99b5754cf52b4ebaf3c5635617aff
Ramunas9 <ramskis9@gmail.com>
2018-04-10 13:43:06 +0300

    GAME-42 Tilemap rendering material fix create enemy melee tank tankM01 attack animation

---
+++ BioDude/Assets/Scripts/Legs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Legs : MonoBehaviour {
+
+    [SerializeField]
+    float rotationSpeed = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // position - set position your tank should go to.
+    public void GoTo(Vector2 positiion)
+    {
+
+    }
+}
---
+++ BioDude/Assets/Scripts/MeleeEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTank : MonoBehaviour {
+
+    private Vector2 headDir;
+    private Vector2 legsDir;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
