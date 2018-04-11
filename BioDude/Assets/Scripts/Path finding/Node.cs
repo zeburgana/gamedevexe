866586838ce28ac0872db0b6c9960981401cf1a4
Ramunas9 <ramskis9@gmail.com>
2018-04-11 22:17:24 +0300

    GAME-42 create node class

---
+++ BioDude/Assets/Scripts/Path finding/Node.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    private Vector2Int coordinates;
+    private int H;
+    private int G;
+    private int F;
+
+}
