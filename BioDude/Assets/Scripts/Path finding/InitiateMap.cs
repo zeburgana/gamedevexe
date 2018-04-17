1c5ec382a529d00417542c29ffaa1cd5dccb950e
Ramunas9 <ramskis9@gmail.com>
2018-04-17 16:27:15 +0300

    GAME-42 start writing path finding algorithm

--- BioDude/Assets/Scripts/Path finding/InitiateMap.cs
+++ BioDude/Assets/Scripts/Path finding/InitiateMap.cs
@@ -3,36 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+
+/// <summary>
+/// Map array values
+/// 0 - floor (none)
+/// 1 - wall (undestroyable)
+/// 
+/// 
+/// </summary>
+
 public class InitiateMap : MonoBehaviour {
 
     public Grid grid;
     public Tilemap wallmap;
-    public TileBase tile;
 
-    /// <summary>
-    /// 0 - floor (none)
-    /// 1 - wall (undestroyable)
-    /// 
-    /// 
-    /// </summary>
-
-    
-    
+    public Vector2Int MapSize;
+    public Vector2Int CoordsOffset;
+    private int[,] Map;
+    public enum MapTile
+    {
+        floor,  // 0
+        wall    // 1
+    }
 
 	void Start () {
-        
         wallmap.CompressBounds();
-        int x, y;
-        x = wallmap.size.x;
-        y = wallmap.size.y;
-        int xoff = wallmap.origin.x;
-        int yoff = wallmap.origin.y;
-        int[,] Map = new int[x, y];
-        for (int i = 0; i < x; i++)
+        MapSize = new Vector2Int(wallmap.size.x, wallmap.size.y);
+        CoordsOffset = new Vector2Int(wallmap.origin.x, wallmap.origin.y);
+        int[,] Map = new int[MapSize.x, MapSize.y];
+        for (int i = 0; i < MapSize.x; i++)
         {
-            for (int j = 0; j < y; j++)
+            for (int j = 0; j < MapSize.y; j++)
             {
-                if (wallmap.HasTile(new Vector3Int(i+xoff, j+yoff, 0)))
+                if (wallmap.HasTile(new Vector3Int(i+ CoordsOffset.x, j+ CoordsOffset.y, 0)))
                 {
                     Map[i, j] = 1;
                 }
@@ -41,7 +44,29 @@ public class InitiateMap : MonoBehaviour {
         //FOR DEBUGING
         //DebugArray(Map);
     }
-    
+
+
+    public int GetMap(int x, int y)
+    {
+        return Map[x, y];
+    }
+
+    public void SetMap(int x, int y, MapTile value)
+    {
+        Map[x, y] = (int) value;
+    }
+
+    /// <summary>
+    /// Gets World to Grid form origin position
+    /// </summary>
+    /// <param name="position">World position on map</param>
+    /// <returns>GRid based position of object</returns>
+    public Vector2Int GetCoordinates(Vector3 position)        //   ////////////////////////////////// BUG: if position is out of range error is thrown needs fixing
+    {
+        Vector3Int pos = grid.WorldToCell(position);
+        return new Vector2Int(pos.x + CoordsOffset.x, pos.y + CoordsOffset.y);
+    }
+
     ///FOR DEBUGING
     void DebugArray(int[,] Map)
     {
--- BioDude/Assets/Scripts/Path finding/Node.cs
+++ BioDude/Assets/Scripts/Path finding/Node.cs
@@ -5,26 +5,18 @@ using UnityEngine.Tilemaps;
 
 public class Node : MonoBehaviour {
 
+    //FOR CODING 
+    // jei reiks skirtigiem algoritmams skirtingu node reiks padaryt abstraccia klase node su posicija ir algoritmai grazins ta bazine klase
     private Vector2Int coordinates;
-    private int H;
-    private int G;
+    //private int H; //atstumas iki pabaigos
+    //private int G; //atstumas nuo start
     private int F;
-
-
-    //[SerializeField]
-    //Grid grid;
-    //public Tilemap wallmap;
-
-    private void Start()
+    public Node next;
+    public Node parent;
+    
+    public Node(Vector2Int coordinates, int value, Node parent = null, Node next = null)
     {
+
     }
-
-    //private void Update()
-    //{
-    //    Vector3Int cords = grid.WorldToCell(transform.position);
-    //    cords.z = 0;
-    //    Debug.Log(cords);
-    //    Debug.Log(wallmap.HasTile(cords));
-    //}
-
+    
 }
