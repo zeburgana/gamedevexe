cb89ff0f21452f344e783755e8629d20d5fde6ba
Ramunas9 <ramskis9@gmail.com>
2018-04-17 13:47:26 +0300

    GAME-42 create intiate gameobject for map initialisation map array construction while initialising

---
+++ BioDude/Assets/Scripts/Path finding/InitiateMap.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class InitiateMap : MonoBehaviour {
+
+    public Grid grid;
+    public Tilemap wallmap;
+    public TileBase tile;
+
+    /// <summary>
+    /// 0 - floor (none)
+    /// 1 - wall (undestroyable)
+    /// 
+    /// 
+    /// </summary>
+
+    
+    
+
+	void Start () {
+        
+        wallmap.CompressBounds();
+        int x, y;
+        x = wallmap.size.x;
+        y = wallmap.size.y;
+        int xoff = wallmap.origin.x;
+        int yoff = wallmap.origin.y;
+        int[,] Map = new int[x, y];
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                if (wallmap.HasTile(new Vector3Int(i+xoff, j+yoff, 0)))
+                {
+                    Map[i, j] = 1;
+                }
+            }
+        }
+        //FOR DEBUGING
+        //DebugArray(Map);
+    }
+    
+    ///FOR DEBUGING
+    void DebugArray(int[,] Map)
+    {
+        string arrayDebug = "";
+        for (int j = Map.GetLength(1) - 1; j >= 0; j--)
+        {
+            for (int i = 0; i < Map.GetLength(0); i++)
+            {
+                arrayDebug += Map[i, j].ToString();
+            }
+            arrayDebug += "\n";
+        }
+        Debug.Log(arrayDebug);
+    }
+    /////////////////////////
+}
--- BioDude/Assets/Scripts/Path finding/Node.cs
+++ BioDude/Assets/Scripts/Path finding/Node.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Node : MonoBehaviour {
 
@@ -8,5 +9,22 @@ public class Node : MonoBehaviour {
     private int H;
     private int G;
     private int F;
+
+
+    //[SerializeField]
+    //Grid grid;
+    //public Tilemap wallmap;
+
+    private void Start()
+    {
+    }
+
+    //private void Update()
+    //{
+    //    Vector3Int cords = grid.WorldToCell(transform.position);
+    //    cords.z = 0;
+    //    Debug.Log(cords);
+    //    Debug.Log(wallmap.HasTile(cords));
+    //}
 
 }
