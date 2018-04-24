ddb1894ff567a0c60f67551a8557f2987ab16921
Marius Teleisa <marius.teleisa@gmail.com>
2018-04-24 17:48:11 +0300

    GAME-52
    updated from new dev branch.
    Created misile script. It has velocity and angular speed variables. The object that has the script follows mouse cursor. The projectile functions the same way as bullet currently

---
+++ BioDude/Assets/Scripts/Items scripts/GuidedMisile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class GuidedMisile : MonoBehaviour {
+
+    public float speed = 5f;
+    public float rotSpeed = 200f;
+
+    private Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = -10; // select distance = 10 units from the camera
+        Vector2 direction = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - body.position;
+
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+
+        body.angularVelocity = -rotateAmount * rotSpeed;
+
+        body.velocity = transform.up * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Enemy" ||
+            collision.collider.tag == "Player")
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+        if (collision.collider.tag == "Wallmap")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
