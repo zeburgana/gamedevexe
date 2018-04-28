428e5de1f0ae9f36f46f9881d1a91d64b07d9e50
Ramunas9 <ramskis9@gmail.com>
2018-04-29 01:16:20 +0300

    GAME-42 add enmey attack animation and attack damage

--- BioDude/Assets/Scripts/Enemy scripts/MeleeTank.cs
+++ BioDude/Assets/Scripts/Enemy scripts/MeleeTank.cs
@@ -6,16 +6,22 @@ using UnityEngine;
 
 public class MeleeTank : MonoBehaviour
 {
+    //this should inherit base class with damage and other variables and maybe script for damaging player
+    public int damage = 1;
     
     public Transform head;
     MeleeHead headScript;
-
     public GameObject player;
+
+    Animator animator;
+
+    private bool TargetInRange = false;
 
 	// Use this for initialization
 	void Start () {
         //head = transform.GetChild(1);
         headScript = head.GetComponent<MeleeHead>();
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -28,5 +34,31 @@ public class MeleeTank : MonoBehaviour
     {
         Vector2 direction = player.transform.position - transform.position;
         headScript.targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            TargetInRange = true;
+            animator.SetBool("TargetInRange", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            TargetInRange = false;
+            animator.SetBool("TargetInRange", false);
+        }
+    }
+
+    private void PerformAttack() // a smarter way of inflicting damage is required - this is nether secure nor efficient 
+    {
+        if (TargetInRange)
+        {
+            player.GetComponent<PlayerHealthManager>().HurtPlayer(damage);
+        }
     }
 }
