ea9bd043f25e90235a41e964fadb69f73b3bca59
Ramunas9 <ramskis9@gmail.com>
2018-04-30 17:55:20 +0300

    GAME-42 add enemy main ai logic

--- BioDude/Assets/Scripts/Enemy scripts/MeleeHead.cs
+++ BioDude/Assets/Scripts/Enemy scripts/MeleeHead.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class MeleeHead : MonoBehaviour {
 
     public float targetAngle;
-    public float rotationSpeed = 0.5f;
+    public float rotationSpeed = 1f;
     public bool canRotate = true;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(canRotate)
+        if (canRotate)
+        {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, targetAngle), rotationSpeed);
+        }
     }
 }
--- BioDude/Assets/Scripts/Enemy scripts/MeleeTank.cs
+++ BioDude/Assets/Scripts/Enemy scripts/MeleeTank.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 // NOTE: Children order is important fro this script to work
 
@@ -12,35 +13,140 @@ public class MeleeTank : MonoBehaviour
     public Transform head;
     MeleeHead headScript;
     public GameObject player;
+    public bool isAlerted = false;
+    public float visionAngle = 30; // vision angle
+    public float visionRange = 10;
+    public LayerMask obstacleMask;
+    public Transform lastPositionTargetSeen;
+    public float localSearchRadius = 5; // distance - how far to search for player from last known position
+    public int localSearchTryLocation = 3; // how many locations to try after target lost
+    IAstarAI ai;
+
+    //private:
+    public bool targetInVision = false;
+    public float distanceToPlayer;
+    private bool targetInAttackRange = false;
+    private AIDestinationSetter aiDestinationSetter;
+    private Patrol aiPatrol;
+    private bool prevTargetInVision = false;
 
     Animator animator;
-
-    private bool TargetInRange = false;
 
 	// Use this for initialization
 	void Start () {
         //head = transform.GetChild(1);
         headScript = head.GetComponent<MeleeHead>();
         animator = GetComponent<Animator>();
+        aiDestinationSetter = GetComponent<AIDestinationSetter>();
+        aiPatrol = GetComponent<Patrol>();
+        ai = GetComponent<IAstarAI>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        SetHeadDirection();
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+        //Set head target direction:
+        if (targetInVision)
+            headScript.targetAngle = VectorToAngle(direction);
+        else
+            headScript.targetAngle = VectorToAngle(transform.up); // this should always set head rotation to zero. better solution required
+
+        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        targetInVision = false;
+        if (distanceToPlayer < visionRange) // if in range
+        {
+            if (Vector3.Angle(head.transform.up, direction) < visionAngle) // if in vision angle
+            {
+                if(!Physics2D.Raycast(transform.position, direction, distanceToPlayer, obstacleMask)) //if no obstacles in between
+                {
+                    targetInVision = true;
+                    lastPositionTargetSeen.position = player.transform.position; // Needs to be updated because other tanks might not see player and would attempt to go to last known location
+                }
+            }
+        }
+        if (prevTargetInVision != targetInAttackRange)
+            VisionStatusChange();
+        prevTargetInVision = targetInVision;
+        if(isAlerted && !targetInVision) // pursuing to last known location
+        {
+            if(ai.reachedEndOfPath) // went to last known location and player not seen
+            {
+                PerformLocalSearch();
+                ReturnToPatrol();
+            }
+        }
+    }
+    
+
+    /// <summary>
+    /// reikia susikurti kintamaji kuriame bus saugoma vieta apie kuria vykdys paieska
+    /// reikia update metode ideti kad jei isAllerted ir is searching ir jei kas nors mato player tai destination player..
+    /// 
+    /// </summary>
+    private void PerformLocalSearch()
+    {
+        Vector2 RandomLocation = new Vector2();
     }
 
-    //reiktu padaryti kad ne kiekviena frame o reciau skaiciuotu
-    private void SetHeadDirection()
+    //Stop pursuing player
+    public void ReturnToPatrol()
     {
-        Vector2 direction = player.transform.position - transform.position;
-        headScript.targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+        PerformLocalSearch();
+        aiDestinationSetter.enabled = false;
+        aiPatrol.enabled = true;
+        isAlerted = false;
+    }
+
+    //call this method to make enemy go to last known player position
+    public void PursuePlayer()
+    {
+        isAlerted = true;
+        aiPatrol.enabled = false;
+        aiDestinationSetter.enabled = true;
+        aiDestinationSetter.target = lastPositionTargetSeen;
+    }
+
+    private void VisionStatusChange()
+    {
+        if (isAlerted)
+        {
+            if (targetInVision) // can see on its own now
+            {
+                aiDestinationSetter.target = player.transform;
+            }
+            else // can't see anymore
+            {
+                aiDestinationSetter.target = lastPositionTargetSeen;
+                //check if last known pos reached? then go to patrol
+
+            }
+        }
+        else
+        {
+            if (targetInVision)
+            {
+                PursuePlayer();
+                aiDestinationSetter.target = player.transform;
+            }
+        }
+        
+    }
+
+    // these might be placed in more general location
+    private float VectorToAngle(Vector2 vect)
+    {
+        return Mathf.Atan2(vect.y, vect.x) * Mathf.Rad2Deg - 90;
+    }
+    private Vector2 AngleToVector(float angle)
+    {
+        return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            TargetInRange = true;
+            targetInAttackRange = true;
             animator.SetBool("TargetInRange", true);
         }
     }
@@ -49,14 +155,14 @@ public class MeleeTank : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-            TargetInRange = false;
+            targetInAttackRange = false;
             animator.SetBool("TargetInRange", false);
         }
     }
 
     private void PerformAttack() // a smarter way of inflicting damage is required - this is nether secure nor efficient 
     {
-        if (TargetInRange)
+        if (targetInAttackRange)
         {
             player.GetComponent<PlayerHealthManager>().HurtPlayer(damage);
         }
