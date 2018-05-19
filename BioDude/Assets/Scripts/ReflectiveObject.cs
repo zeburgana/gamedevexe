98564dd0cf581919b16fe5fe8dfc389cc8f62c90
Aidas Subačius <aidaso2@yahoo,com>
2018-05-19 16:49:34 +0300

    GAME-50
    Added bouncy wall, and not working rotation before switching branches

--- BioDude/Assets/Scripts/ReflectiveObject.cs
+++ BioDude/Assets/Scripts/ReflectiveObject.cs
@@ -4,30 +4,52 @@ using UnityEngine;
 
 public class ReflectiveObject : MonoBehaviour {
 
-
+    public Vector3 currDir;
     public LayerMask collisionMask;
+    public float maxStepDistance = 200;
+    private LayerMask wallMask;
 
+
+    public void Awake()
+    {
+        wallMask = LayerMask.GetMask("Collider");
+    }
     // Use this for initialization
     void Start () {
-        
+
     }
 	
 	// Update is called once per frame
 	void Update () {
     }
 
+
+    private float VectorToAngle(Vector2 vect)
+    {
+        return Mathf.Atan2(vect.y, vect.x) * Mathf.Rad2Deg - 90;
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Bullet")
         {
-            Debug.Log(collision.transform);
-            Ray2D ray = new Ray2D(collision.transform.position, collision.transform.forward);
-            RaycastHit2D hit;
-            if (Physics2D.Raycast(ray, hit,5000.0f, collisionMask))
+            
+            Debug.DrawRay(collision.transform.position, collision.transform.up, Color.green, 1f);
+            Ray ray = new Ray(collision.transform.position, collision.transform.up);
+            RaycastHit2D bulletHitWall = Physics2D.Raycast(collision.transform.position, collision.transform.up, Mathf.Infinity, wallMask);
+            if (bulletHitWall.collider != null)
             {
-
+                Debug.Log(bulletHitWall.collider.name);
+                Debug.Log(bulletHitWall.normal);
+                var rotation = Vector2.Reflect(collision.transform.GetComponent<Rigidbody2D>().velocity, bulletHitWall.normal);
+                Debug.Log(rotation);
+                collision.transform.rotation = Quaternion.Euler(0f, 0f, VectorToAngle(rotation)) ;
+                Debug.Log(Quaternion.Euler(0f, 0f, VectorToAngle(rotation)));
             }
-            Vector2 reflectDir = Vector2.Reflect(ray.direction, hit.normal);
+            else Debug.Log("null");
+            
+
         }
     }
 }
