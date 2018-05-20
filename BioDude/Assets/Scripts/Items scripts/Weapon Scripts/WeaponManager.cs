f891852edb148d650e8ff1121c75b9090d9c5e3a
Ramunas9 <ramskis9@gmail.com>
2018-05-20 16:29:00 +0300

    GAME-77 automatic firing for assault rifle

--- BioDude/Assets/Scripts/Items scripts/Weapon Scripts/WeaponManager.cs
+++ BioDude/Assets/Scripts/Items scripts/Weapon Scripts/WeaponManager.cs
@@ -106,6 +106,8 @@ public class WeaponManager : MonoBehaviour
     private RawImage explosiveImage;
     private RawImage weaponSlotImage;
 
+    private int[] AutomaticWeapons = { 2 };
+
 
     // Use this for initialization
     void Start ()
@@ -394,6 +396,18 @@ public class WeaponManager : MonoBehaviour
         UpdateBulletGUI();
         UpdateWeaponGUI();
         isReloading = false;
+    }
+
+    public void AutomaticShoot()
+    {
+        for(int i = 0; i < AutomaticWeapons.Length; i++)
+        {
+            if (AutomaticWeapons[i] == selectedFireArm)
+            {
+                Shoot();
+                break;
+            }
+        }
     }
 
     public void Shoot()
