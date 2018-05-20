bec89b8db248c71f87a47382a818e7118b17b441
Ramunas9 <ramskis9@gmail.com>
2018-05-20 15:41:43 +0300

    hotfix dualwielded weapon shooting

--- BioDude/Assets/Scripts/Items scripts/Weapon Scripts/WeaponManager.cs
+++ BioDude/Assets/Scripts/Items scripts/Weapon Scripts/WeaponManager.cs
@@ -499,7 +499,7 @@ public class WeaponManager : MonoBehaviour
         float bulletAngle = Random.Range(-weaponScript.accuracy, weaponScript.accuracy);
         GameObject newBullet = Instantiate(aWeaponScript.projectile, activeWeaponRTip.transform.position, Quaternion.Euler(0f, 0f, activeWeaponRTip.transform.rotation.eulerAngles.z + bulletAngle), projectiles);
         newBullet.GetComponent<Bullet>().Instantiate(aWeaponScript.timeUntilSelfDestrucion, aWeaponScript.projectileSpeed, aWeaponScript.damage);
-        if (aWeaponScript.clipSize > 1) // if only one bullet left in clip so two guns can't fire
+        if (aWeaponScript.currentClipAmmo > 1) // if only one bullet left in clip so two guns can't fire
         {
             GameObject newBullet2 = Instantiate(aWeaponScript.projectile, activeWeaponLTip.transform.position, Quaternion.Euler(0f, 0f, activeWeaponLTip.transform.rotation.eulerAngles.z + bulletAngle), projectiles);
             newBullet2.GetComponent<Bullet>().Instantiate(aWeaponScript.timeUntilSelfDestrucion, aWeaponScript.projectileSpeed, aWeaponScript.damage);
