48dd75fe1ce2c711aab41530db2dd0cadb01ac47
Ramunas9 <ramskis9@gmail.com>
2018-05-23 13:05:22 +0300

    finished unit test

--- BioDude/Assets/Tests/RamunasTestas.cs
+++ BioDude/Assets/Tests/RamunasTestas.cs
@@ -16,19 +16,41 @@ public class RamunasTestas {
     }
 
     [Test]
-	public void InstatiatedWithTag()
+    public void InstatiatedWithTag()
     {
-        Assert.AreEqual(expectedBullet.tag, bullet.tag, "Bullet failed {0} ", bullet.transform.position.ToString());
+        Assert.AreEqual(expectedBullet.tag, bullet.tag, "Wrong Bullet Tag {0} ", bullet.transform.position.ToString());
     }
 
-    //[UnityTest]
-    //public IEnumerator RTestWithEnumeratorPasses()
-    //{
-    //    WeaponManager.ShootPistol();
-    //    Debug.Log("before");
-    //    yield return new WaitForSecondsRealtime(8);
-    //    Debug.Log("after");
-    //}
+    [Test]
+    public void rigidbodyTest() {
+        Rigidbody2D rigidbody2D = bullet.GetComponent<Rigidbody2D>();
+        Rigidbody2D expected = expectedBullet.GetComponent<Rigidbody2D>();
+        Assert.AreEqual(expected.angularDrag, rigidbody2D.angularDrag, "Wrong Angular Drag");
+        Assert.AreEqual(expected.angularVelocity, rigidbody2D.angularVelocity, "Wrong Angular Velocity");
+        Assert.AreEqual(expected.collisionDetectionMode, rigidbody2D.collisionDetectionMode, "Wrong collisionDetectionMode");
+        Assert.AreEqual(expected.constraints, rigidbody2D.constraints, "Wrong constrains");
+        Assert.AreEqual(expected.gravityScale, rigidbody2D.gravityScale, "Wrong gravity scale");
+        Assert.AreEqual(expected.isKinematic, rigidbody2D.isKinematic, "Wrong is Kinematic");
+    }
+
+    [Test]
+    public void bulletDamageChaged ()
+    {
+        Assert.AreNotEqual(expectedBullet.GetComponent<Bullet>().damage, bullet.GetComponent<Bullet>().damage, "Wrong Bullet damage");
+    }
+
+    [Test]
+    public void BulletDamageSet()
+    {
+        Assert.AreEqual(bullet.GetComponent<Bullet>().damage, WeaponManager.weaponArray[0].GetComponent<Weapon>().damage, "Wrong damage set");
+    }
+
+    [Test]
+    public void randomRotationgained()
+    {
+        Assert.GreaterOrEqual(bullet.transform.rotation.z,- WeaponManager.weaponArray[0].GetComponent<Weapon>().accuracy + WeaponManager.transform.rotation.z, "Wring rotation");
+        Assert.LessOrEqual(bullet.transform.rotation.z, WeaponManager.weaponArray[0].GetComponent<Weapon>().accuracy+ WeaponManager.transform.rotation.z, "Wring rotation");
+    }
 
     [SetUp]
     public void RamunasSetUp()
@@ -48,42 +70,29 @@ public class RamunasTestas {
     [TearDown] 
     public void RamunasTearDown()
     {
+        Debug.Log("teardown");
         Object.Destroy(bullet);
     }
     
 }
 
-//public class WeaponEquipTests
+
+//[UnityTest]
+//public IEnumerator BulletFlyDistance()
 //{
-
-//    [UnityTest]
-//    public IEnumerator WeaponEquipTestWithEnumeratorPasses()
-//    {
-//        var ToBeThrownObject = Resources.Load("Testing/grenade");
-//        var objectlauncher = new GameObject().AddComponent<player>();
-//        objectlauncher.TestSetSelectedGrenadeTo(ToBeThrownObject);
-//        objectlauncher.GetComponent<player>().UseGrenade();
-//        //yield return new WaitForSeconds(1);
-//        var spawnedObject = GameObject.FindWithTag("Explosive");
-//        Debug.Log(spawnedObject);
-//        var ThrownObject = PrefabUtility.GetPrefabParent(spawnedObject);
-//        Debug.Log(ThrownObject);
-//        Assert.AreEqual(ToBeThrownObject, ThrownObject);
-//        yield return new WaitForSeconds(1);
-//    }
-
-//    [TearDown]
-//    public void AfterEveryTest()
-//    {
-//        foreach (var gameObject in GameObject.FindObjectsOfType<Explosive>())
-//        {
-//            Object.Destroy(gameObject);
-//        }
-//        foreach (var gameObject in Object.FindObjectsOfType<player>())
-//        {
-//            Object.Destroy(gameObject);
-//        }
-
-//    }
-
+//    Debug.Log("before");
+//    int time = 1;
+//    float expdist = WeaponManager.weaponArray[0].GetComponent<Weapon>().projectileSpeed * time;
+//    Vector2 startingpos = bullet.transform.position;
+//    Debug.Log(expdist);
+//    yield return new WaitForSecondsRealtime(time);
+//    Debug.Log("after");
+//    Vector2 endingpos = bullet.transform.position;
+//    Assert.AreEqual(expdist, Vector2.Distance(startingpos, endingpos), "Wrong distance");
+//}
+//[UnityTest]
+//public IEnumerator BulletDestroyed()
+//{
+//    yield return new WaitForSecondsRealtime(WeaponManager.weaponArray[0].GetComponent<Weapon>().timeUntilSelfDestrucion);
+//    Assert.AreEqual(< null >, bullet, "Bullet not destroyed");
 //}
