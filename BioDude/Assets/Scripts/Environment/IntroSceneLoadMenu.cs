44b49f620b06b91c30998b83f836f981dfd1d8fd
Margiris <margiris.burakauskas@gmail.com>
2018-05-28 04:01:09 +0300

    GAME-97
    Created a new scene to play the intro video and a script to handle switching to main menu. Added the intro video.

---
+++ BioDude/Assets/Scripts/Environment/IntroSceneLoadMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroSceneLoadMenu : MonoBehaviour
+{
+	private bool isPlaying;
+
+	// Use this for initialization
+	void Start ()
+	{
+		isPlaying = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
