4f3bc10357612b698eba250e0d6cda8a9c4b9ce6
Ramunas <ramskis9@gmail.com>
2019-02-21 20:13:47 +0200

    Initial AIDude branch

--- BioDude/Assets/Scripts/Enemy scripts/Tank.cs
+++ BioDude/Assets/Scripts/Enemy scripts/Tank.cs
@@ -305,7 +305,7 @@ public abstract class Tank : Character
         else
         {
             ai.maxSpeed = normalSpeed;
-            alertionIndicatorSpriteRenderer.sprite = new Sprite();
+            alertionIndicatorSpriteRenderer.sprite = null;//new Sprite();
             alertionIndicatorAnimator.SetFloat("Speed", 0);
         }
     }
